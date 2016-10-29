2643f40964426275f00799727d92df29d1086299
梦宇 <xtiqin@163.com>
2016-10-29 20:51:58 +0800

    remove blank lines

--- src/views/index/Index.js
+++ src/views/index/Index.js
@@ -1,7 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 const RecordStatus = require("./RecordStatus");
 const electron = require("electron");
 
@@ -142,7 +138,6 @@ class Index {
         this._btnStartOrStop.disabled = true;
         this._btnSave.disabled = true;
     }
-
 }
 
 new Index();
